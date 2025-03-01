b8ee6da66e041c5588f69de9df5fedbed8470d6e
Andri Muhayat <muhayatandri@gmail.com>
2025-03-02 02:25:48 +0700

    add: server js and mysql conn

--- internal/platform/database/mysql/mysql.js
+++ internal/platform/database/mysql/mysql.js
@@ -1,13 +1,17 @@
 const knex = require("knex");
+const dotenv = require('dotenv');
+dotenv.config();
+
 const cfg = {
     client: 'mysql',
     connection: {
         host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 7001,
         user: process.env.DB_USER || 'root',
         password: process.env.DB_PASS || 'password',
         database: process.env.DB_NAME || 'school_db',
     }
 }
-
+console.log(process.env.DB_PASS)
 const db = knex(cfg);
 module.exports = db;
--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const teacherRoutes = require('./internal/module/teacher/handler');
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swagger');
 
+require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
-
-app.use('/api', teacherRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
