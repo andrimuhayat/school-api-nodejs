c863bbc71fb91c68e189ccac59e236c893300552
Andri Muhayat <muhayatandri@gmail.com>
2025-03-02 16:44:39 +0700

    add: swagger docs

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const teacherRoutes = require('./internal/module/teacher/route');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
 
 require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/api', teacherRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
